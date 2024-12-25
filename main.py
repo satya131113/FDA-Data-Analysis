9f895a35eb9ce7e8880d306c715b5e1d806abee3
satya131113 <satyamuralikrishna13@gmail.com>
2024-12-25 02:22:46 -0700

    Fixed issue with deployment

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from dash import Dash
 from data import read_data
 from plots import create_countries_plot
-import dash_labs as dl
 from layout import create_layout
 
-# Initialize the Dash app
 app = Dash(__name__, title="FDA Data Analysis")
 
-# Read the data
+
 drug_recall_data = read_data('./datafiles/drug-enforcement-data.csv')
 
-# Set up the layout
-app.layout = create_layout(dataframe=drug_recall_data)
+app.layout = create_layout(dataframe= drug_recall_data)
+
+# Expose the Flask server instance for Gunicorn
+server = app.server
 
 if __name__ == '__main__':
-    # Export the app as static files using Dash Labs
-    dl.plugins.export(app, './static')
+    app.run(debug=False)
