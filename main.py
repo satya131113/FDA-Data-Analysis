1ed4d4ba820e6b2284f32475420d98680d4e7f01
satya131113 <satyamuralikrishna13@gmail.com>
2024-12-25 02:12:12 -0700

    Connecting to Heroku

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from dash import Dash
 from data import read_data
 from plots import create_countries_plot
+import dash_labs as dl
 from layout import create_layout
 
+# Initialize the Dash app
 app = Dash(__name__, title="FDA Data Analysis")
 
-
+# Read the data
 drug_recall_data = read_data('./datafiles/drug-enforcement-data.csv')
 
-app.layout = create_layout(dataframe= drug_recall_data)
+# Set up the layout
+app.layout = create_layout(dataframe=drug_recall_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Export the app as static files using Dash Labs
+    dl.plugins.export(app, './static')
